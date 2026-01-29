b9b290f9a4aa9c23fa3c58ffcc2cfa49c3576edd
rama8255 <68927671+rama8255@users.noreply.github.com>
2026-01-28 19:19:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -562,7 +562,3 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init);
-const CONFIG = {
-  SUPABASE_URL: 'https://ovrfmnzacrxgfhumebwv.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_AzHzLucADvr77dDabbiRzw_K_wJZkov'
-};
